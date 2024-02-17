0c2ec539c0149598de53176559ed99a5a98fd131
Raul Albín <raul200794@gmail.com>
2024-02-17 21:06:14 +0100

    improve text analyzer with more exclusions and config options

--- test/writing_tools_test.dart
+++ test/writing_tools_test.dart
@@ -2,6 +2,7 @@
 // ignore_for_file: constant_identifier_names
 
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:test/test.dart';
@@ -20,6 +21,31 @@ const N = '\n';
 const T = '\t';
 const R = '\r';
 const CONTRACTION = "'";
+const OPEN_DIALOGS = "”";
+const CLOSING_DIALOGS = '“';
+const ASKING_MARK_OPEN = "¿";
+const ASKING_MARK_CLOSING = "?";
+
+extension MonoSilabusCheck on String {
+  bool get isMonoSylable => length == 1;
+}
+
+extension BubbleSorting on List<WordStat> {
+  List<WordStat> bubbleSort() {
+    final length = this.length;
+    for (var i = 0; i < length; i++) {
+      for (var j = i + 1; j < length; j++) {
+        WordStat elementA = this[i];
+        WordStat elementB = this[j];
+        if (elementB.repetitions > elementA.repetitions) {
+          this[i] = elementB;
+          this[j] = elementA;
+        }
+      }
+    }
+    return this;
+  }
+}
 
 extension CleanString on String {
   String clean() {
@@ -29,6 +55,10 @@ extension CleanString on String {
         .replaceAll(POINT_COMMA, IDLE)
         .replaceAll(OPENING, CLOSING)
         .replaceAll(CLOSING, IDLE)
+        .replaceAll(CLOSING_DIALOGS, IDLE)
+        .replaceAll(OPEN_DIALOGS, IDLE)
+               .replaceAll(ASKING_MARK_OPEN, IDLE)
+          .replaceAll(ASKING_MARK_CLOSING, IDLE)
         .replaceAll(N, SPACE)
         .replaceAll(R, SPACE)
         .replaceAll(T, SPACE)
@@ -48,8 +78,10 @@ class WordStat {
 }
 
 class ReadStats {
+  //constructor
   ReadStats();
 
+  //atributos
   int _textLength = 0;
   int _totalWords = 0;
   final WordCounterRegistry _wordCounterRegistry = {};
@@ -74,26 +106,53 @@ class ReadStats {
 
   List<WordStat> getMostRepeated(int amount) {
     final registry = {..._wordCounterRegistry};
-    final wordStats = <WordStat>[];
+    var wordStats = <WordStat>[];
     for (final entry in registry.entries) {
       wordStats.add(WordStat(entry.value, entry.key));
     }
-    wordStats.sort((a, b) => a.repetitions < b.repetitions ? 1 : -1);
+    wordStats = wordStats.bubbleSort();
     if (amount > registry.length) {
       return wordStats;
     }
     final sublist = wordStats.sublist(0, amount);
     return sublist;
   }
+
+  WordStat getStatsForWord(String word){
+    try{
+      final count = registry[word]!;
+      return WordStat(count, word);
+    }catch(e){
+      return WordStat(0, word);
+    }
+  }
 }
 
 class TextAnalyzer {
+  // atributos
   final String path;
-  TextAnalyzer(this.path);
   bool caseSensitive = false;
-  File get file => File(path);
-
+  bool excludeMonosilables = false;
+  int minCharactersNeededToRegister = 1;
+  // constructor
+  TextAnalyzer(this.path);
+  // métodos
+  bool _shouldExcludeWordFromCounting(String word) {
+    if (word.isEmpty) {
+      return true;
+    }
+    if (word.isMonoSylable && excludeMonosilables) {
+      return true;
+    }
+    if (word.length < minCharactersNeededToRegister) {
+      return true;
+    }
+    return false;
+  }
   Future<ReadStats> analyze() async {
+    assert(minCharactersNeededToRegister > 0, '''
+      minCharactersNeededToRegister must be greater than 0
+    ''');
     final readStats = ReadStats();
     final file = File(path);
 
@@ -104,11 +163,16 @@ class TextAnalyzer {
 
     final stream = file.openRead();
     await for (final data in stream) {
-      String string = String.fromCharCodes(data);
+      final encoding = Utf8Decoder(allowMalformed: true);
+      final decodedBytes = encoding.convert(data);
+      String string = decodedBytes;
       //process
       string = string.clean();
       final split = string.split(SPACE);
       for (var word in split) {
+        if (_shouldExcludeWordFromCounting(word)) {
+          continue;
+        }
         if (!caseSensitive) {
           word = word.toLowerCase();
         }
@@ -147,7 +211,7 @@ void main() {
       expect(wordMap['ipsum'], 4);
       expect(wordMap['it'], 3);
 
-      expect(totalWords, 92);
+      expect(totalWords, 91);
     });
     test('use longer text file with known words', () async {
       final path = 'test/test_3.txt';
@@ -159,18 +223,25 @@ void main() {
       print(readStats.textLength);
       print(readStats.getMostRepeated(5));
       print(readStats.registry['aliquam']);
-      expect(totalWords, 505);
+      expect(totalWords, 500);
     });
 
     test('analyze El Camino de los Reyes', () async {
-      final path = 'test/test_4.txt';
+      var path = 'test/test_4.txt';
+      path = 'docs/el_camino_reyes_ESP.txt';
       final textAnalyzer = TextAnalyzer(path);
+      
+      textAnalyzer.excludeMonosilables = false;
+      textAnalyzer.minCharactersNeededToRegister = 1;
       final readStats = await textAnalyzer.analyze();
       final totalWords = readStats.totalWords;
-
       print(totalWords);
-      final deCount = readStats.registry['de'];
-      print(deCount);
+
+      final wordStat = readStats.getStatsForWord('esquirlada');
+      print(wordStat);
+
+      final repeated = readStats.getMostRepeated(20);
+      print(repeated);
     });
 
     test('analyze LPDA', () async {
